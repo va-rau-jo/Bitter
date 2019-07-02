3444e341cf215e709bc6e7c5687b8910047015fc
Victor Araujo <varaujo@fb.com>
2019-07-01 17:44:06 -0700

    finished basic tweet display

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
@@ -2,21 +2,31 @@ package com.codepath.apps.restclienttemplate.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.codepath.apps.restclienttemplate.BitterApp;
 import com.codepath.apps.restclienttemplate.BitterClient;
 import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.TweetAdapter;
+import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActivity extends AppCompatActivity {
 
     private BitterClient client;
+    private ArrayList<Tweet> tweets;
+    private TweetAdapter adapter;
+    private RecyclerView rvTweets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,12 @@ public class TimelineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline);
 
         client = BitterApp.getRestClient(this);
+        tweets = new ArrayList<>();
+        adapter = new TweetAdapter(tweets);
+        rvTweets = findViewById(R.id.rvTweets);
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(adapter);
+
         populateTimeline();
     }
 
@@ -31,27 +47,40 @@ public class TimelineActivity extends AppCompatActivity {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                Log.d("success", "made it ");
+                Log.d("success", "made it 1");
             }
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                Log.d("success", "made it ");
+                try {
+                    for(int i = 0; i < response.length(); i++) {
+                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
+                        tweets.add(tweet);
+                        adapter.notifyItemInserted(tweets.size() - 1);
+                    }
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.d("success", "made it 2");
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                Log.d("EPIC fail", "made it ");
+                Log.d("TwitterClient", "uh oh spaghetti-os");
+                throwable.printStackTrace();
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
-                Log.d("EPIC fail", "made it ");
+                Log.d("TwitterClient", "uh oh spaghetti-os");
+                throwable.printStackTrace();
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                Log.d("EPIC fail", "made it ");
+                Log.d("TwitterClient", "uh oh spaghetti-os");
+                throwable.printStackTrace();
             }
         });
     }
